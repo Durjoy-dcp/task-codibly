1b74739784fa8b7674e86be936dd5d558cd08185
Durjoy-dcp <unknownccxv@gmail.com>
2023-01-17 16:01:56 +0600

    sync done with input

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,37 @@ import "./App.css";
 import { DataEntity, Iproducts } from "./model/Iproducts";
 
 function App() {
-  const [data, setData] = React.useState<Iproducts>({} as Iproducts);
   const [products, setProducts] = React.useState<DataEntity[]>([]);
+  const [currentProducts, setCurrentProducts] = React.useState<DataEntity[]>(
+    []
+  );
 
   useEffect(() => {
     fetch(`https://reqres.in/api/products`)
       .then((res) => res.json())
       .then((data) => {
-        setData(data);
         setProducts(data?.data);
+        setCurrentProducts(data?.data);
       });
   }, []);
+  const showResult = (id: number | string) => {
+    if (!id) {
+      setCurrentProducts(products);
+      return;
+    }
+    const newData = products.filter((data) => data.id === id);
+    setCurrentProducts(newData);
+  };
 
   return (
     <div className="App" style={{ maxWidth: "600px", margin: "auto" }}>
+      <input
+        type="number"
+        onKeyUp={
+          //@ts-ignore
+          (event) => showResult(Number(event.target.value))
+        }
+      />
       <table>
         <thead>
           <tr>
@@ -27,8 +44,8 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          {products.map((data, id) => (
-            <tr key={id}>
+          {currentProducts.map((data, id) => (
+            <tr key={id} style={{ backgroundColor: `${data.color}` }}>
               <td>{data.id}</td>
               <td>{data.name}</td>
               <td>{data.year}</td>
